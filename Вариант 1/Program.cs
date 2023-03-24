d187062fd00220006a52e7e9eb90e88e5a273a6d
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-03-25 02:59:57 +0300

    первый вариант

--- Вариант 1/Program.cs
+++ Вариант 1/Program.cs
@@ -4,3 +4,25 @@
 // "1234", "1567", "-2", "computer science"
 // "aaaa", "bbbb", "cccc"
 
+string[] array1 = new string[5] {":-j", "03", "hello", "world", "aaaa"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[i] = array1[i];
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
