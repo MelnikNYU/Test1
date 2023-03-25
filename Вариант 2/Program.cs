93819f7cdb7c87c7a25958e3221e526eb314c64d
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-03-25 03:05:42 +0300

    второй вариант

---
+++ Вариант 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая из имеющегося массива формирует массив 
+//из элементов менее 3 символов.
+// "hello", "world", "2", ":-)"
+// "1234", "1567", "-2", "computer science"
+// "aaaa", "bbbb", "cccc"
+
+var elements = new string[] { "hello", "world", "-2", ":-)" };
+ 
+string[] group = Array.FindAll(elements, element => element.Length < 4);
+
+foreach (var element in group) Console.WriteLine(element);
+
